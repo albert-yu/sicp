e3cc3d0794bc1749a6f8ee68abc82562fedf6675
Albert Yu <albert-yu@users.noreply.github.com>
2019-02-12 13:40:55 -0600

    Remove multiply-iter

--- ch-1/ex-1-17.scm
+++ ch-1/ex-1-17.scm
@@ -6,18 +6,6 @@
 (define (double n)
   (+ n n))
 
-
-(define (multiply-iter a b total)
-  (cond ((= 0 b) total)
-        ;; ((= 1 b) (+ total a))
-        ;; ((= 2 b) (+ total (double a)))
-        ((even? b) (multiply-iter a (halve b) (+ (double total) a)))
-        (else (multiply-iter a (- b 1) (+ total a)))))
-
-
-;; (define (multiply-fast a b)
-;;   (multiply-iter a b 0))
-
 (define (multiply-fast a b)
   (cond ((= 0 b) 0)
         ((even? b) (double (multiply-fast a (halve b))))

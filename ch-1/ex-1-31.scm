ee2f7e5f95074e6d7d13936d22fcd8a77df01127
Albert Yu <albert.yu@relativity.com>
2019-02-15 15:45:12 -0600

    Calculate pi, too

--- ch-1/ex-1-31.scm
+++ ch-1/ex-1-31.scm
@@ -16,8 +16,21 @@
     (* (product-recur term (next a) next b) (term a))))
 
 
-;; test
+;; calculate pi
+(define (pi-next a)
+  (+ a 2))
 
+
+(define (pi-term t)
+  (* (/ (- t 1.0) t) (/ (+ t 1.0) t)))
+
+
+(define (calc-pi accuracy)
+  ;; exact->inexact converts the fraction to a decimal
+  (* 4 (product-iter pi-term 3 pi-next accuracy)))
+
+
+;; calculate factorial
 (define (id n)
   n)
 
@@ -33,6 +46,7 @@
 (define (factorial-recur n)
   (product-recur id 1 inc n))
 
+;; test
 
 (define (puts expr)
   (display expr (current-output-port))
@@ -42,3 +56,6 @@
 (puts (factorial-recur 10))
 
 (puts (factorial-iter 10))
+
+(puts (calc-pi 1000000))
+

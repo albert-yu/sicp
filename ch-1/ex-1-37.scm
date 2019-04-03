ae3a22c43d06a790c24eafbeac25cdf1e9911a37
Albert Yu <albert.yu@gmail.com>
2019-04-03 15:29:48 -0500

    Getting phi instead of 1/phi

--- ch-1/ex-1-37.scm
+++ ch-1/ex-1-37.scm
@@ -4,21 +4,41 @@
   (display expr (current-output-port))
   (newline (current-output-port)))
 
+;; a)
 
-(define (cont-frac-iter n d k i)
+(define (cont-frac-recur n d k i)
   (if (= i k) 
     (/ (n i) (d i))
     (/ 
       (n i) 
-      (+ (d i) (cont-frac-iter n d k (+ 1 i))))))
+      (+ (d i) (cont-frac-recur n d k (+ 1 i))))))
     
 
-
 (define (cont-frac n d k)
-  (cont-frac-iter n d k 1))
+  (cont-frac-recur n d k 1))
 
 
 (puts (cont-frac (lambda (i) 1.0)
 		 (lambda (i) 1.0)
 		 1000))
 
+
+;; b)
+
+(define (cont-frac-iter n d k i state)
+  (if (= i k)
+    (+
+      state
+      (/ (n i) (d i)))
+    (cont-frac-iter n d k (+ i 1) (/ 
+				    (n (+ i 1))
+				    (+ (d (+ i 1)) state)))))
+
+
+(define (cont-frac-iterative n d k)
+  (cont-frac-iter n d k 1 0))
+
+
+(puts (cont-frac-iterative (lambda (i) 1.0)
+			   (lambda (i) 1.0)
+			   1000))

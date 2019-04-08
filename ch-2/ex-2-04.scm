a5ade1242ab7999d96fd9ced24e65207d47dee9b
Albert Yu <albert.yu@gmail.com>
2019-04-07 21:19:38 -0700

    Add Ex. 2.4

---
+++ ch-2/ex-2-04.scm
@@ -0,0 +1,19 @@
+(define (puts expr)
+  (display expr (current-output-port))
+  (newline (current-output-port)))
+
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+
+(puts (car (cons 12 3)))
+
+
+
